43b9ca1015f7790cbad390b6978153b83f713c63
Xenation <xenations.111@gmail.com>
2018-12-15 15:17:22 +0100

    Basic Swarm Movement

---
+++ Assets/Scripts/PlayerSwarm.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Swarm {
+	public class PlayerSwarm : MonoBehaviour {
+
+		public float cursorSpeed = 5f;
+		public float cursorRadius = 1f;
+		public GameObject unitPrefab;
+		public int unitsToCreate = 50;
+		public float unitSpeed = 4f;
+		public float unitRadius = .1f;
+
+		public bool debug = false;
+
+		[System.NonSerialized] public List<PlayerUnit> units = new List<PlayerUnit>();
+		[System.NonSerialized] public Transform cursor;
+
+		private Vector2 velocity;
+
+		private void Awake() {
+			for (int i = 0; i < unitsToCreate; i++) {
+				Instantiate(unitPrefab, new Vector3(Random.Range(-5f, 5f), Random.Range(-5f, 5f)), Quaternion.identity, transform);
+			}
+			cursor = transform.Find("Cursor");
+			GetComponentsInChildren(units);
+		}
+
+		private void Update() {
+			velocity.x = Input.GetAxisRaw("Horizontal");
+			velocity.y = Input.GetAxisRaw("Vertical");
+			velocity.Normalize();
+			velocity *= cursorSpeed;
+		}
+
+		private void FixedUpdate() {
+			cursor.position += (Vector3) velocity * Time.fixedDeltaTime;
+		}
+
+	}
+}
---
+++ Assets/Scripts/PlayerUnit.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using Xenon;
+
+namespace Swarm {
+	[RequireComponent(typeof(Rigidbody2D))]
+	public class PlayerUnit : MonoBehaviour {
+
+		private Rigidbody2D rb;
+		private PlayerSwarm swarm;
+
+		private Vector2 velocity;
+
+		private void Awake() {
+			rb = GetComponent<Rigidbody2D>();
+			swarm = GetComponentInParent<PlayerSwarm>();
+		}
+
+		private void Update() {
+			Vector2 toCursor = swarm.cursor.position - transform.position;
+			float cursorDistance = toCursor.magnitude;
+			velocity = toCursor.normalized;
+			if (cursorDistance < swarm.cursorRadius) {
+				velocity *= cursorDistance / swarm.cursorRadius;
+			}
+			velocity *= swarm.unitSpeed;
+			DebugVelocity(velocity, Color.green);
+
+			float sqrUnitRaduis = swarm.unitRadius * swarm.unitRadius;
+			Vector2 displace = Vector2.zero;
+			foreach (PlayerUnit unit in swarm.units) {
+				if (unit == this) continue;
+				Vector2 toOther = unit.transform.position - transform.position;
+				if (toOther.sqrMagnitude < sqrUnitRaduis) {
+					displace -= toOther.normalized * (1f - toOther.sqrMagnitude / sqrUnitRaduis);
+				}
+			}
+			DebugVelocity(displace, Color.yellow);
+			
+			velocity += displace;
+
+			DebugVelocity(velocity, Color.blue);
+		}
+
+		private void FixedUpdate() {
+			rb.velocity = velocity;
+		}
+
+		private void DebugVelocity(Vector2 vel, Color col) {
+			if (!swarm.debug) return;
+			Debug.DrawLine(transform.position, transform.position + (Vector3) vel, col);
+		}
+
+	}
+}
