0944552a6edd9b3680c75d822b2b65b0233d7655
Damoone <ambjo@hotmail.fr>
2018-12-16 16:57:24 +0100

    Correct a name

--- Assets/Walls.cs
+++ Assets/Walls.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class Walls : MonoBehaviour
 {
-	Camera camera;
+	Camera cameraMain;
     // Start is called before the first frame update
     void Start()
     {
-		camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+		cameraMain = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 		float screenHeightInUnits = Camera.main.orthographicSize ;
 		float screenWidthInUnits = (screenHeightInUnits * Screen.width / Screen.height);
-		transform.GetChild(0).position = new Vector3(camera.transform.position.x - screenWidthInUnits-0.5f, camera.transform.position.y, transform.GetChild(0).position.z);
-		transform.GetChild(1).position = new Vector3(camera.transform.position.x + screenWidthInUnits+0.5f, camera.transform.position.y, transform.GetChild(0).position.z);
-		transform.GetChild(2).position = new Vector3(camera.transform.position.x , camera.transform.position.y + screenHeightInUnits + 0.5f, transform.GetChild(0).position.z);
-		transform.GetChild(3).position = new Vector3(camera.transform.position.x, camera.transform.position.y - screenHeightInUnits - 0.5f, transform.GetChild(0).position.z);
+		transform.GetChild(0).position = new Vector3(cameraMain.transform.position.x - screenWidthInUnits-0.5f, cameraMain.transform.position.y, transform.GetChild(0).position.z);
+		transform.GetChild(1).position = new Vector3(cameraMain.transform.position.x + screenWidthInUnits+0.5f, cameraMain.transform.position.y, transform.GetChild(0).position.z);
+		transform.GetChild(2).position = new Vector3(cameraMain.transform.position.x , cameraMain.transform.position.y + screenHeightInUnits + 0.5f, transform.GetChild(0).position.z);
+		transform.GetChild(3).position = new Vector3(cameraMain.transform.position.x, cameraMain.transform.position.y - screenHeightInUnits - 0.5f, transform.GetChild(0).position.z);
 	}
 
     // Update is called once per frame
