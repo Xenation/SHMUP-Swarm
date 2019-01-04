4d0c2471bc12a6c826daea628be7360212d4ad49
Unknown <jeremyguarober@hotmail.fr>
2019-01-04 16:17:29 +0100

    NewSound pour coller à la DA et équilibrage sfx music

--- Assets/Scripts/ProjectileUnit.cs
+++ Assets/Scripts/ProjectileUnit.cs
@@ -24,6 +24,7 @@ namespace Swarm {
                 //Add vibration
 				Destroy(gameObject);
                 //SON DE MORT A L'IMPACT EN MODE KAMIKAZE
+                AkSoundEngine.PostEvent("Play_Death", gameObject);
 			}
 		}
 
