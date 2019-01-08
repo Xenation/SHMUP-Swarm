15cc10519441a49c4051970bdfe9cee34419bd37
MagxmaDUCK <magxm41@gmail.com>
2019-01-08 20:09:34 +0100

    Vibrations change according to position of pyu to cursor

--- Assets/Scripts/KillOnCollision.cs
+++ Assets/Scripts/KillOnCollision.cs
@@ -6,6 +6,7 @@ namespace Swarm {
     {
 
         private CircleCollider2D bossCollider;
+        private GameObject player;
         private PlayerSwarm swarm;
 
         //Controller
@@ -21,6 +22,10 @@ namespace Swarm {
         private void Start()
         {
             bossCollider = GetComponent<CircleCollider2D>();
+
+            player = GameObject.Find("PlayerSwarm");
+            swarm = player.GetComponent<PlayerSwarm>();
+            
         }
 
         private void FixedUpdate()
@@ -51,7 +56,21 @@ namespace Swarm {
             if (pu)
             {
                 AkSoundEngine.PostEvent("Play_Death", gameObject);
-                GamePad.SetVibration(pIndex, vibrationStrength, vibrationStrength);
+
+                //Vector2 diff = swarm.cursor.transform.position - pu.transform.position;
+
+                float distBetweenPointsRight = Mathf.Sqrt(Mathf.Pow(pu.transform.position.x - swarm.cursor.position.x + 0.5f, 2) + Mathf.Pow(pu.transform.position.y - swarm.cursor.position.y, 2));
+                float distBetweenPointsLeft = Mathf.Sqrt(Mathf.Pow(pu.transform.position.x - swarm.cursor.position.x - 0.5f, 2) + Mathf.Pow(pu.transform.position.y - swarm.cursor.position.y, 2));
+
+                float ratioRight = 1 - distBetweenPointsRight / 8.0f; //Divisé par le ratio;
+                float ratioLeft = 1 - distBetweenPointsLeft / 8.0f;
+
+                float vibrationStrRight = vibrationStrength * ratioRight;
+                float vibrationStrLeft = vibrationStrength * ratioLeft;
+
+
+
+                GamePad.SetVibration(pIndex, vibrationStrLeft, vibrationStrRight);
                 Destroy(collision.gameObject);
                 startVib = Time.time;
                 Invoke("StopVibration", vibrationDuration);
