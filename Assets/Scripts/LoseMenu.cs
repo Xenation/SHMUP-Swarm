324de02bf95511e5b94b5e94f4a1a3a9f25ce498
MagxmaDUCK <magxm41@gmail.com>
2019-01-08 18:04:46 +0100

    Working menus

---
+++ Assets/Scripts/LoseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("returnToMenu").GetComponent<Button>().onClick.AddListener(onClickMenu);
+        GameObject.Find("Retry").GetComponent<Button>().onClick.AddListener(onClickRetry);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void onClickMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void onClickRetry()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
---
+++ Assets/Scripts/winMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class winMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.Find("returnToMenu").GetComponent<Button>().onClick.AddListener(onClickMenu);
+        GameObject.Find("SubmitScore").GetComponent<Button>().onClick.AddListener(onClickSubmit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void onClickMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    void onClickSubmit()
+    {
+        //Mettre le score en ligne
+    }
+}
