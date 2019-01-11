858591c69bea1e8b52d10065cf659bbd64116f52
MagxmaDUCK <magxm41@gmail.com>
2019-01-11 12:20:40 +0100

    moving eye

--- Assets/Scripts/bossLife.cs
+++ Assets/Scripts/bossLife.cs
@@ -3,171 +3,222 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class bossLife : MonoBehaviour
+namespace Swarm
 {
-	[SerializeField]
-	private int pv;
-    private int phase2Threshhold;
-    private int phase3Threshhold;
-	public bool isPart = true;
-	private Animator animator;
-    public Camera cam;
-
-	public float openingDuration= 5;
-	private float openingTime = 2;
-	private bool isAnimationEnd= false;
-
-    private bool inHitStun = false;
-    private Material mat;
-
-    public float hitStunDuration = 0.05f;
-    private float hitStunFirstFrame = 0;
-
-    private float ScoreTimer = 0;
-
-    //Ajouter une référence vers chaque part du boss
-
-
-    private void Awake()
+    public class bossLife : MonoBehaviour
     {
-        ScoreTimer = Time.time;
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-		animator = gameObject.GetComponent<Animator>();
-        SpriteRenderer rend = gameObject.GetComponent<SpriteRenderer>();
-        mat = rend.material;
-    }
+        [SerializeField]
+        private int pv;
+        private int phase2Threshhold;
+        private int phase3Threshhold;
+        public bool isPart = true;
+        private Animator animator;
+        public Camera cam;
 
-    // Update is called once per frame
-    void Update()
-    {
-		if (!isPart && isAnimationEnd && Time.time - openingTime > openingDuration)
-		{
-			animator.SetBool("isOpen", false);
-			isPart = true;
-            isAnimationEnd = false;
+        public GameObject eye;
+        public float maxEyeDist= 0.5f;
+        public PlayerSwarm player;
+        private Vector3 eyeOrigin;
+        private Vector3 eyeOpenOrigin;
+        private Vector3 previousPos;
+        private Vector3 futurePos;
+        private int frames = 0;
 
+        public float openingDuration = 5;
+        private float openingTime = 2;
+        private bool isAnimationEnd = false;
+
+        private bool inHitStun = false;
+        private Material mat;
+
+        public float hitStunDuration = 0.05f;
+        private float hitStunFirstFrame = 0;
+
+        private float ScoreTimer = 0;
+
+        //Ajouter une référence vers chaque part du boss
+
+
+        private void Awake()
+        {
+            ScoreTimer = Time.time;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            animator = gameObject.GetComponent<Animator>();
+            SpriteRenderer rend = gameObject.GetComponent<SpriteRenderer>();
+            mat = rend.material;
+            eyeOrigin = eye.transform.position;
+            eyeOpenOrigin = eyeOrigin + new Vector3(0, 1.20f, 0); // 1.20   //0.8 __ 1.93   -0.6f
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!isPart && isAnimationEnd && Time.time - openingTime > openingDuration)
+            {
+                animator.SetBool("isOpen", false);
+                isPart = true;
+                isAnimationEnd = false;
+
+                foreach (GameObject part in GameObject.FindGameObjectsWithTag("part"))
+                {
+                    part.GetComponent<partController>().resetPart();
+                }
+
+                CheckPhase();
+            }
+            GameObject head = GameObject.Find("Head");
+            if (isPart)
+            {
+                Vector3 eyeDirection = Vector3.zero;
+                eyeDirection.x = player.cursor.transform.position.x - eyeOrigin.x;
+                eyeDirection.y = player.cursor.transform.position.y - eyeOrigin.y;
+                //eyeDirection.z = player.cursor.transform.position.z - eyeOrigin.z;
+
+                eyeDirection.Normalize();
+                eyeDirection *= maxEyeDist;
+                eye.transform.position = head.transform.position + eyeDirection;
+            }
+            else
+            {
+                if(frames <= 1)
+                {
+                    frames++;
+                    eye.transform.position = new Vector3(Mathf.Lerp(eye.transform.position.x, futurePos.x, 0.5f), Mathf.Lerp(eye.transform.position.y, futurePos.y, 0.5f), eye.transform.position.z);
+                }
+                else
+                {
+                    previousPos = eye.transform.position;
+
+                     Vector3 eyeDirection = Vector3.zero;
+                     eyeDirection.x = Random.Range(-maxEyeDist, maxEyeDist);
+                     eyeDirection.y = Random.Range(-maxEyeDist, maxEyeDist);
+                    //eyeDirection.x = Random.Range(-maxEyeDist, maxEyeDist);
+
+                    //GameObject head = GameObject.Find("Head");
+                     futurePos =  head.transform.position + eyeDirection;
+                    futurePos.y = futurePos.y ;
+                     frames = 0;
+                }
+               
+            }
+
+            if (inHitStun == true)
+            {
+                hitstun();
+            }
+        }
+
+        void OnCollisionEnter2D(Collision2D col)
+        {
+            if (!isPart && isAnimationEnd && col.gameObject.layer == LayerMask.NameToLayer("ProjectileUnit"))
+            {
+                lowerPV();
+                hitstun();
+            }
+        }
+
+        void lowerPV()
+        {
+            pv--;
+            if (pv <= 0)
+            {
+
+                Invoke("End", 1.0f);
+
+                //Destroy(this.gameObject);
+            }
+            else
+            {
+                //INSERER SON DEGAT SUR BOSS
+                AkSoundEngine.PostEvent("Play_CoeurHit", gameObject);
+            }
+        }
+
+        public void End()
+        {
+            ScoreTimer = Time.time - ScoreTimer;
+            //Envoyez le score dans la prochaine scene + leaderboard
+
+
+            SceneManager.LoadScene("Win");
+
+        }
+
+        public void checkParts()
+        {
+            isPart = false;
             foreach (GameObject part in GameObject.FindGameObjectsWithTag("part"))
-			{
-				part.GetComponent<partController>().resetPart();
-			}
+            {
+                if (!part.GetComponent<partController>().isDestroyed)
+                {
+                    isPart = true;
+                }
+            }
 
-            CheckPhase();
-		}
+            if (!isPart)
+            {
+                openingTime = Time.time;
+                foreach (GameObject part in GameObject.FindGameObjectsWithTag("part"))
+                {
+                    animator.SetBool("isOpen", true);
+                    //part.SetActive(false);
+                    AkSoundEngine.PostEvent("Play_BossOpen", gameObject);
+                }
+            }
+        }
 
-        if (inHitStun == true)
+        private void hitstun()
         {
-            hitstun();
+            if (!inHitStun)
+            {
+                inHitStun = true;
+                mat.SetFloat("_ReplaceAmount", 1.0f);
+
+                hitStunFirstFrame = Time.time;
+            }
+            else if (Time.time > (hitStunFirstFrame + hitStunDuration))
+            {
+                inHitStun = false;
+                mat.SetFloat("_ReplaceAmount", 0.0f);
+                //cam.transform.position =new Vector3(0, 0, -10);
+            }
+            else
+            {
+                //ScreenShake
+                cam.transform.position += new Vector3(Random.Range(-0.05f, 0.05f), Random.Range(-0.05f, 0.05f));
+
+
+            }
+        }
+
+        private void CheckPhase()
+        {
+            if (pv < phase3Threshhold)
+            {
+                //Change sprite and patterns to second phase
+            }
+            else if (pv < phase2Threshhold)
+            {
+                //Change sprite and patterns to third phase
+            }
+        }
+
+        private void OnDestroy()
+        {
+            //Debug.Log("died");
+            //SceneManager.LoadScene("Win");
+        }
+
+        public void AlertObservers(string message)
+        {
+            if (message.Equals("AttackAnimationEnded"))
+            {
+                isAnimationEnd = true;
+            }
         }
     }
 
-	void OnCollisionEnter2D(Collision2D col)
-	{
-		if (!isPart && isAnimationEnd && col.gameObject.layer == LayerMask.NameToLayer("ProjectileUnit"))
-		{
-			lowerPV();
-            hitstun();
-		}
-	}
-
-	void lowerPV()
-	{
-		pv--;
-		if (pv <= 0)
-		{
-            
-            Invoke("End", 1.0f);
-
-            //Destroy(this.gameObject);
-        }
-        else
-        {
-            //INSERER SON DEGAT SUR BOSS
-            AkSoundEngine.PostEvent("Play_CoeurHit", gameObject);
-        }
-	}
-
-    public void End()
-    {
-        ScoreTimer = Time.time - ScoreTimer;
-        //Envoyez le score dans la prochaine scene + leaderboard
-        
-
-        SceneManager.LoadScene("Win");
-        
-    }
-
-    public void checkParts()
-	{
-		isPart = false;
-		foreach (GameObject part in GameObject.FindGameObjectsWithTag("part"))
-		{
-			if (!part.GetComponent<partController>().isDestroyed)
-			{
-				isPart = true;
-			}
-		}
-
-		if (!isPart)
-		{
-			openingTime = Time.time;
-			foreach (GameObject part in GameObject.FindGameObjectsWithTag("part"))
-			{
-				animator.SetBool("isOpen",true);
-                //part.SetActive(false);
-                AkSoundEngine.PostEvent("Play_BossOpen", gameObject);
-			}
-		}
-	}
-
-    private void hitstun()
-    {
-        if (!inHitStun)
-        {
-            inHitStun = true;
-            mat.SetFloat("_ReplaceAmount", 1.0f);
-            
-            hitStunFirstFrame = Time.time;
-        }
-        else if (Time.time > (hitStunFirstFrame + hitStunDuration))
-        {
-            inHitStun = false;
-            mat.SetFloat("_ReplaceAmount", 0.0f);
-            //cam.transform.position =new Vector3(0, 0, -10);
-        }
-        else
-        {
-            //ScreenShake
-            cam.transform.position += new Vector3(Random.Range(-0.05f, 0.05f), Random.Range(-0.05f, 0.05f));
-            
-
-        }
-    }
-
-    private void CheckPhase()
-    {
-        if(pv < phase3Threshhold)
-        {
-            //Change sprite and patterns to second phase
-        }else if(pv < phase2Threshhold)
-        {
-            //Change sprite and patterns to third phase
-        }
-    }
-
-    private void OnDestroy()
-    {
-        //Debug.Log("died");
-        //SceneManager.LoadScene("Win");
-    }
-
-	public void AlertObservers(string message)
-	{
-		if (message.Equals("AttackAnimationEnded"))
-		{
-			isAnimationEnd = true;
-		}
-	}
 }
