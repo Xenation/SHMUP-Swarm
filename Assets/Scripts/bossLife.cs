b8b757e2ce4dfa39ab792cfc9d9b43f6899ce071
MagxmaDUCK <magxm41@gmail.com>
2019-01-17 10:41:56 +0100

    WIP boss end anim restarts

--- Assets/Scripts/bossLife.cs
+++ Assets/Scripts/bossLife.cs
@@ -155,6 +155,8 @@ namespace Swarm
 
             if (pv <= 0)
             {
+                Collider2D bossColl = GetComponent<Collider2D>();
+                bossColl.enabled = false;
                 bossExpl.SetTrigger("explode");
 				isDying = true;
 				dieStartTime = Time.time;
