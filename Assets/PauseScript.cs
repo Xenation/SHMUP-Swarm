a39cb102ce6e1d190f69e40765ae67f1cc4ce527
MagxmaDUCK <magxm41@gmail.com>
2019-01-19 20:36:43 +0100

    Working Pause Menu

--- Assets/PauseScript.cs
+++ Assets/PauseScript.cs
@@ -14,6 +14,7 @@ namespace Swarm
         public Button continueButton;
         public Button menuButton;
         private Button prevButton;
+        private bool firstFrame = false;
 
         void Start()
         {
@@ -30,6 +31,7 @@ namespace Swarm
             EventSystem.current.SetSelectedGameObject(continueButton.gameObject, null);
             prevButton = continueButton;
             continueButton.Select();
+            firstFrame = true;
         }
 
         // Update is called once per frame
@@ -38,16 +40,36 @@ namespace Swarm
             if(Input.GetAxis("Vertical") > 0.1f)
             {
                 EventSystem.current.SetSelectedGameObject(continueButton.gameObject, null);
+                continueButton.Select();
                 prevButton = continueButton;
             }
             else if(Input.GetAxis("Vertical") < -0.1f)
             {
                 EventSystem.current.SetSelectedGameObject(menuButton.gameObject, null);
+                menuButton.Select();
                 prevButton = menuButton;
             }
             else
             {
                 EventSystem.current.SetSelectedGameObject(prevButton.gameObject, null);
+                prevButton.Select();
+
+                if(prevButton == continueButton && firstFrame)
+                {
+                    EventSystem.current.SetSelectedGameObject(menuButton.gameObject, null);
+                    menuButton.Select();
+                    EventSystem.current.SetSelectedGameObject(prevButton.gameObject, null);
+                    prevButton.Select();
+                    firstFrame = false;
+                }
+                else if(firstFrame)
+                {
+                    EventSystem.current.SetSelectedGameObject(continueButton.gameObject, null);
+                    continueButton.Select();
+                    EventSystem.current.SetSelectedGameObject(prevButton.gameObject, null);
+                    prevButton.Select();
+                    firstFrame = false;
+                }
             }
         }
 
