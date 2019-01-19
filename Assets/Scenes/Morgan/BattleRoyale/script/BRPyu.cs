9e47b1cad187891ef43faa7f3cb8f8be1c6f55d2
MagxmaDUCK <magxm41@gmail.com>
2019-01-19 21:19:34 +0100

    merge

---
+++ Assets/Scenes/Morgan/BattleRoyale/script/BRPyu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BRPyu : MonoBehaviour
+{
+    public string team;
+    public Tournament tournament;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(BRPyu pyu in tournament.contenders)
+        {
+            if (team != pyu.team)
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scenes/Morgan/BattleRoyale/script/Tournament.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tournament : MonoBehaviour
+{
+    [HideInInspector] public List<BRPyu> contenders;
+
+    public int nbOfContendersPerTeam = 5;
+
+    public string[] Teams;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
